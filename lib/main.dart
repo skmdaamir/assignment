bb01315d08d8b77ff098b3511cb461bc47fb887b
skmdaamir <skmdaamir29@gmail.com>
2021-01-29 22:38:51 +0530

    ch4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:assignment/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:assignment/pages/home_page.dart';
 
 import 'pages/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Awesome App",
-    home: HomePage(),
+    home: LoginPage(),
     theme: ThemeData(
       primarySwatch: Colors.purple,
     ),
+    routes: {
+      "/Login": (context) => LoginPage(),
+      "/home": (context) => HomePage(),
+    },
   ));
 }
